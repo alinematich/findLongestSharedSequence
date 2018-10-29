ed49616aa0289961e0dc605b51c57ac5e0487ec3
alinemati <alinemati@live.com>
2018-10-30 03:07:38 +0330

    #3 solve token limits

--- findSequences.py
+++ findSequences.py
@@ -7,20 +7,30 @@ import sys
 
 tokenize = lambda string: list(map(lambda token: token.value, javalang.tokenizer.tokenize(string+'\n')))
 
-def longestCommonSequences(codes):
-    scores = {}
+def longestCommonSequencesLimited(codes, scores, minLength, maxLength):
     for i, code in enumerate(codes):
-        print(i, filenames[i], len(code))
-        for length in range(2, min([500, len(code)+1])):
+        if len(code) < minLength:
+            continue
+        print(i, filenames[i], len(code), minLength, maxLength)
+        for length in range(max([2, minLength]), min([maxLength, len(code)+1])):
             for index in range(0, len(code) - length + 1):
-                tpl = hash(tuple(code[index:index+length]))
+                tpl = ' '.join(code[index:index+length])
                 scores[tpl] = scores.get(tpl, 0) + 1
             if length % 100 == 0:
                 print(length)
     scores = {k: v for k, v in scores.items() if v > 1}
-    # scores.sort(key=lambda item: item[0], reverse= True)
-    # scores = [{'score': (math.log2(len(k)) * math.log2(v)),'count': v ,'seq': k} for k, v in scores.items() if v > 1]
-    # scores.sort(key=lambda item: item['score'], reverse= True)
+    return scores
+
+def longestCommonSequences(codes):
+    block = 100
+    scores = {}
+    for i in range(block, max(list(map(lambda code: len(code), codes)))+block, block):
+        scores = longestCommonSequencesLimited(codes, scores, i - block, i + 1)
+        print(max(list(map(lambda item: len(item.split()), scores.keys()))))
+        if max(list(map(lambda item: len(item.split()), scores.keys()))) < i:
+            break
+    scores = [{'score': (math.log2(len(k.split())) * math.log2(v)),'count': v ,'seq': k.split()} for k, v in scores.items() if v > 1]
+    scores.sort(key=lambda item: item['score'], reverse= True)
     return scores
 
 def getAllFiles(path, ext):
@@ -39,36 +49,7 @@ for filename in filenames:
     with open(filename) as infile:
         codes += [tokenize(infile.read())]
 
-# with open(sys.argv[2], mode='w') as outfile:
-#     writer = csv.writer(outfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#     writer.writerow(['Score', 'Tokens', 'Count', 'Source Code'])
-#     writer.writerows(list(map(lambda item: [item['score'], len(item['seq']), item['count'], list(item['seq'])], longestCommonSequences(codes))))
-
-def saveToCsv(hashedResult):
-    seen = set()
-    f = open(sys.argv[2], mode='w')
-    writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+with open(sys.argv[2], mode='w') as outfile:
+    writer = csv.writer(outfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     writer.writerow(['Score', 'Tokens', 'Count', 'Source Code'])
-    finished = False
-    for i, code in enumerate(codes):
-        print(i, filenames[i], len(code))
-        for length in range(2, min([500, len(code)+1])):
-            for index in range(0, len(code) - length + 1):
-                tp = code[index:index+length]
-                hs = hash(tuple(tp))
-                if hs not in seen and hs in hashedResult:
-                    seen.add(hs)
-                    val = hashedResult[hs]
-                    writer.writerow([(math.log2(len(tp)) * math.log2(val)), len(tp), val, tp])
-                    if len(seen) == len(hashedResult):
-                        finished = True
-                        break
-            if length % 100 == 0:
-                print(length)
-            if finished:
-                break
-        if finished:
-            break
-    f.close()
-
-saveToCsv(longestCommonSequences(codes))
+    writer.writerows(list(map(lambda item: [item['score'], len(item['seq']), item['count'], list(item['seq'])], longestCommonSequences(codes))))
