2ee7c8834de8b21a6b2808b0fc9ce9d32cda7694
alinemati <alinemati@live.com>
2018-10-29 19:02:33 +0330

    #3 make the dictionary

--- findSequences.py
+++ findSequences.py
@@ -3,6 +3,20 @@ from os import listdir
 from os.path import isfile, isdir, join
 import javalang
 
+tokenize = lambda string: list(map(lambda token: token.value, javalang.tokenizer.tokenize(string+'\n')))
+
+def longestCommonSequences(codes):
+    scores = {}
+    for code in codes:
+        for length in range(2, len(code)+1):
+            for index in range(0, len(code) - length + 1):
+                tpl = tuple(code[index:index+length])
+                scores[tpl] = scores.get(tpl, 0) + 1
+
+    scores = [{'score': (math.log2(len(k)) * math.log2(v)),'count': v ,'seq': k} for k, v in scores.items() if v > 1]
+    scores.sort(key=lambda item: item['score'], reverse= True)
+    return scores
+
 def getAllFiles(path, ext):
     files = []
     for f in listdir(path):
@@ -13,23 +27,9 @@ def getAllFiles(path, ext):
             files += getAllFiles(fn, ext)
     return files
 filenames = getAllFiles('../repos/java-design-patterns/abstract-document/src/', 'java')
-
 codes = []
 for filename in filenames:
     with open(filename) as infile:
-        codes += [list(map(lambda token: token.value, javalang.tokenizer.tokenize(infile.read()+'\n')))]
+        codes += [tokenize(infile.read())]
 
-scores = {}
-for code in codes:
-    for length in range(2, len(code)+1):
-        for index in range(0, len(code) - length + 1):
-            tpl = tuple(code[index:index+length])
-            scores[tpl] = scores.get(tpl, 0) + 1
-
-scores = [((math.log2(len(k)) * math.log2(v)) , k) for k, v in scores.items() if v > 1]
-scores.sort(key=lambda item: item[0], reverse= True)
-
-for i in scores[:100]:
-    print(i)
-    print()
-    print('------------------------------#####')
+print(longestCommonSequences(codes))
