c1104cc4c9a738e7cb308713f74bc9cf4f71d8f2
alinemati <alinemati@live.com>
2018-10-30 03:57:12 +0330

    #3 further optimizations

--- findSequences.py
+++ findSequences.py
@@ -7,31 +7,39 @@ import sys
 
 tokenize = lambda string: list(map(lambda token: token.value, javalang.tokenizer.tokenize(string+'\n')))
 
-def longestCommonSequencesLimited(codes, scores, minLength, maxLength):
+def longestCommonSequencesLimited(codes, scores, minLength, maxLength, ignoredfiles):
+    scores={}
     for i, code in enumerate(codes):
-        if len(code) < minLength:
+        if len(code) < minLength or i in ignoredfiles:
             continue
         print(i, filenames[i], len(code), minLength, maxLength)
-        for length in range(max([2, minLength]), min([maxLength, len(code)+1])):
+        for length in range(max([2, minLength]), min([maxLength+1, len(code)+1])):
             for index in range(0, len(code) - length + 1):
                 tpl = ' '.join(code[index:index+length])
-                scores[tpl] = scores.get(tpl, 0) + 1
-            if length % 100 == 0:
-                print(length)
-    scores = {k: v for k, v in scores.items() if v > 1}
+                score = scores.get(tpl, (0, frozenset()))
+                scores[tpl] = (score[0] + 1, score[1] | frozenset([i]))
+    scores = {k: v for k, v in scores.items() if v[0] > 1}
+    filesUsed = set()
+    for score, tup in scores.items():
+        for fileUsed in tup[1]:
+            filesUsed.add(fileUsed)
+    for i in set(range(len(codes)))-filesUsed:
+        ignoredfiles.add(i)
     return scores
 
 def longestCommonSequences(codes):
     block = 100
-    scores = {}
-    for i in range(block, max(list(map(lambda code: len(code), codes)))+block, block):
-        scores = longestCommonSequencesLimited(codes, scores, i - block, i + 1)
-        print(max(list(map(lambda item: len(item.split()), scores.keys()))))
-        if max(list(map(lambda item: len(item.split()), scores.keys()))) < i:
+    scores = []
+    ignored = set()
+    for i in range(block, len(max(codes, key=lambda code: len(code)))+block, block):
+        scores += [longestCommonSequencesLimited(codes, scores, i - block, i, ignored)]
+        if len(max(scores[-1].keys(), key=lambda item: len(item.split())).split()) < i:
             break
-    scores = [{'score': (math.log2(len(k.split())) * math.log2(v)),'count': v ,'seq': k.split()} for k, v in scores.items() if v > 1]
-    scores.sort(key=lambda item: item['score'], reverse= True)
-    return scores
+    res = []
+    for score in scores:
+        res += [{'score': (math.log2(len(k.split())) * math.log2(v[0])),'count': v[0] ,'seq': k.split()} for k, v in score.items() if v[0] > 1]
+    res.sort(key=lambda item: item['score'], reverse= True)
+    return res
 
 def getAllFiles(path, ext):
     files = []
